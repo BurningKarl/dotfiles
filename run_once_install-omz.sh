60ff9febe80893d765fa772096c4338f968c4eb0
BurningKarl <10158964+BurningKarl@users.noreply.github.com>
2026-01-17 01:01:39 +0000

    add remaining configs

---
+++ run_once_install-omz.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rc=$?
+if [ ${rc} -eq 1 ]; then
+    # oh-my-zsh is already installed, don't fail
+    exit 0
+fi
+exit ${rc}
---
+++ run_once_set-up-zerotier.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/main/doc/contact%40zerotier.com.gpg' | gpg --import && \
+if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
+
+sudo zerotier-cli status
+sudo zerotier-cli join 17d709436c8769b8
