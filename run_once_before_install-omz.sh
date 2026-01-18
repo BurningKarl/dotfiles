df10ac5c2db1ee6d8d47f6a305d8fc7c96e7f57b
BurningKarl <10158964+BurningKarl@users.noreply.github.com>
2026-01-18 15:37:04 +0000

    fix oh-my-zsh installation

--- run_once_before_install-omz.sh
+++ run_once_before_install-omz.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install zsh
+sudo apt install git zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 rc=$?
 if [ ${rc} -eq 1 ]; then
